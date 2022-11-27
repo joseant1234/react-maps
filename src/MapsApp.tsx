ad0bd3bad332aaea84d241cba6fbdc6ce8af0b55
Jose <jose.antonio.umeres@gmail.com>
2022-11-27 11:17:14 -0500

    Added Loading component and HomeScreen

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,10 +1,11 @@
 import { PlacesProvider } from "./context"
-
+import { HomeScreen } from "./screens"
+import './styles.css'
 
 export const MapsApp = () => {
   return (
     <PlacesProvider>
-        <h1>Hola</h1>
+      <HomeScreen />
     </PlacesProvider>
   )
 }
