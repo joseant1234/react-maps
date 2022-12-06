baacf24c2492d5173c8afcc91ac11fe7fe6ec3c9
Jose <jose.antonio.umeres@gmail.com>
2022-12-05 22:21:06 -0500

    Added a polyline

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,4 +1,4 @@
-import { Map, Marker, Popup } from "mapbox-gl";
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from "mapbox-gl";
 import { useContext, useEffect, useReducer } from "react";
 import { directionsApi } from "../../apis";
 import { DirectionsResponse } from "../../interfaces/directions";
@@ -69,12 +69,66 @@ export const MapProvider = ({ children }: Props) => {
   const getRouteBetweenPoints = async(start: [number, number], end: [number, number]) => {
     const resp = await directionsApi.get<DirectionsResponse>(`/${start.join(',')};${end.join(',')}`);
     const { distance, duration, geometry } = resp.data.routes[0];
+    const { coordinates: coords } = geometry;
+
     let kms = distance / 1000;
     kms = Math.round(kms * 100);
     kms = kms / 100;
 
     const minutes = Math.floor(duration / 60);
-    console.log({ kms, minutes });
+
+    const bounds = new LngLatBounds(
+      start,
+      start
+    );
+
+    for (const coord of coords) {
+      const newCoord: [number, number] = [coord[0], coord[1]];
+      bounds.extend(newCoord);
+    }
+    // con esto el mapa se acomoda para que muestre ambos puntos en el mapa
+    state.map?.fitBounds(bounds, {
+      padding: 200
+    });
+
+    // polyline
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords,
+            }
+          }
+        ]
+      }
+    };
+    // remover polyline si existe, solo se puede tener un elemento con el id
+    if (state.map?.getLayer('RouteString')) {
+      state.map.removeLayer('RouteString');
+      state.map.removeSource('RouteString');
+    }
+    // RouteString es el id de la polyline
+    state.map?.addSource('RouteString', sourceData);
+    // addLayer para que se pinte en el mapa
+    state.map?.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round'
+      },
+      paint: {
+        'line-color': 'black',
+        'line-width': 3
+      }
+    });
   }
 
   return (
